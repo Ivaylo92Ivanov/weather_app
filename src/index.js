aad9aea2d83e6365581ae2bf4e95780a65275a31
Ivaylo Ivanov <ivaylo92ivanov@gmail.com>
2023-11-13 16:30:09 +0200

    Set background

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './style.css'
 
+// to add credits
+//Photo by <a href="https://unsplash.com/@grakozy?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Greg Rakozy</a> on <a href="https://unsplash.com/photos/silhouette-photography-of-person-oMpAz-DN-9I?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+
+
 console.log("works")
